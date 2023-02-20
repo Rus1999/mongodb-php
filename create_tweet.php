04164290e57fee642d2e49a2ffa53d41c4be4b04
Rus <punyarit07@gmail.com>
2023-02-20 15:55:46 +0700

    add create_tweet.php

---
+++ create_tweet.php
@@ -0,0 +1,24 @@
+<?php
+
+    session_start();
+    require_once('dbconnect.php');
+
+    if (!isset($_POST['body']))
+    {
+        exit;
+    }
+
+    $user_id = $_SESSION['user'];
+    $userData = $db->users->findOne(array('_id' => $uesr_id));
+    $body = substr($_POST['body'], 140);
+    $date = date('Y-m-d H:i:s');
+
+    $db->tweets->insertOne(array(
+        'authorId' => $user_id,
+        'authorNmae' => $userData['username'],
+        'body' => $body,
+        'created' => $date
+    ));
+
+    header('Location: home.php');
+?>
